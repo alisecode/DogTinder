0049517bed3e10ed945b0c612ce64da1c7642d75
Alisa <alisa.melissa@icloud.com>
2024-01-12 13:12:46 +0200

    Initial Commit

---
+++ DogTinder/DogTinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogTinderApp.swift
+//  DogTinder
+//
+//  Created by Алиса Третьякова on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DogTinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

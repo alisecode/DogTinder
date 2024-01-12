0439d19fc27dccf45c43dc62a7337eba5a648a87
Alisa <alisa.melissa@icloud.com>
2024-01-12 17:48:03 +0200

    Tinder for Dogs commit

---
+++ DogTinder/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  DogTinder
+//
+//  Created by Алиса Третьякова on 12.01.2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ DogTinder/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  DogTinder
+//
+//  Created by Алиса Третьякова on 12.01.2024.
+//
+
+import Foundation
